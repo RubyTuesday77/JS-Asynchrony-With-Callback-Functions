dcff4271daa919b25a11f337892693e5d9026270
RubyTuesday77 <105758468+RubyTuesday77@users.noreply.github.com>
2022-08-08 21:33:08 -0400

    first commit

--- index.js
+++ index.js
@@ -4,8 +4,22 @@ const pc = newPlayableCharacter(100, 110)
 // Create a non-playable character
 const npc = newNonPlayableCharacter(50, 300)
 
-// have the NPC start walking east immediately
-npc.walkEast()
+// Set up character's path 
+npc.walkNorth(1400, () => {
+    npc.walkEast(1200, () => {
+        npc.walkSouth(300, () => {
+            npc.walkEast(1500, () => {
+                npc.walkSouth(1500, () => {
+                    npc.walkWest(2700, () => {
+                        npc.walkNorth(400)
+                    })
+                })
+            })
+        })
+    })                                                 
+})
+
+
 
 // Create the inventory
 const inventory = newInventory()
